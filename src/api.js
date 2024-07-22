a82cf2122600284ea6e3e2c607ac6d108c7857ab
Maria Gopkalo <marus.space@gmail.com>
2024-07-22 16:32:26 +0300

    update request headers

--- src/api.js
+++ src/api.js
@@ -12,6 +12,17 @@ const fetchData = async ({ url, method, data }) => (
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
       'Authorization': authorizationHeader,
+      'Accept': '*/*',
+      'Accept-Language': 'en-RU,en;q=0.9,ru-RU;q=0.8,ru;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+      'Priority': 'u=1, i',
+      'Sec-Ch-Ua': '\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"',
+      'Sec-Ch-Ua-Mobile': '?1',
+      'Sec-Ch-ua-Platform': '\"Android\"',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-site',
+      'Referer': 'https://hamsterkombatgame.io/',
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
     },
     body: data ? JSON.stringify(data) : undefined,
   }).then((response) => response.json())
