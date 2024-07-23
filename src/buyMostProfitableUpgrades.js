25bc0b42a84e8ea4d81dde1bb4b93fd181ca953b
Maria Gopkalo <marus.space@gmail.com>
2024-07-23 09:55:31 +0300

    add getMostProfitableUnavailableUpgrades

--- src/buyMostProfitableUpgrades.js
+++ src/buyMostProfitableUpgrades.js
@@ -2,6 +2,81 @@ const { fetchData } = require('./api');
 const { getRandomInRange } = require('./getRandomInRange');
 const { formatTime } = require('./formatTime');
 
+const filterByConditionType = (objects) => {
+  const recursiveFilter = (obj) => {
+    if ((obj._type && obj._type !== 'ByUpgrade') || obj.isExpired) {
+      return null;
+    }
+
+    if (obj.condition) {
+      const nestedResult = recursiveFilter(obj.condition);
+
+      if (!nestedResult) {
+        return null;
+      }
+
+      obj.condition = nestedResult;
+    }
+
+    return obj;
+  };
+
+  return objects
+    .map(obj => recursiveFilter(obj))
+    .filter(obj => obj !== null);
+};
+
+const getMostProfitableUnavailableUpgrades = async () => {
+  try {
+    const { upgradesForBuy } = await fetchData({ url: 'upgrades-for-buy' });
+
+    const getUpgradeById = (upgradeId) => {
+      let upgrade = upgradesForBuy.find(({ id }) => id === upgradeId);
+      
+      upgrade = {
+        ...upgrade,
+        condition: {
+          ...upgrade.condition,
+          ...(upgrade.condition?.upgradeId && getUpgradeById(upgrade.condition.upgradeId)),
+        },
+      };
+
+      if (upgrade.isAvailable) {
+        let { condition, ...otherParams } = upgrade;
+        upgrade = otherParams;
+      }
+      
+      const { name, section, level, price, isExpired, condition, profitPerHourDelta } = upgrade;
+
+      return {
+        currentLevel: level - 1,
+        name,
+        section,
+        price,
+        isExpired,
+        passiveCoinPrice: (price / profitPerHourDelta).toFixed(2),
+        profitPerHourDelta,
+        condition,
+      };
+    };
+
+    const unavailableCardsArray = upgradesForBuy
+        .filter(({ isAvailable, isExpired }) => !isAvailable && !isExpired)
+        .sort((a, b) => b.profitPerHourDelta / b.price - a.profitPerHourDelta / a.price)
+        .filter(({condition}) => condition._type === 'ByUpgrade')
+        .map(({ id }) => getUpgradeById(id));
+    
+    const filteredCards = filterByConditionType(unavailableCardsArray);
+    
+    console.log(JSON.stringify(filteredCards, null, '\t'));
+
+  } catch (error) {
+    console.error('\nОшибка при получении карточек:', error);
+
+    return [];
+  }
+};
+
 const getMostProfitableUpgrades = async ({
   maxPrice,
   cardsCount,
@@ -125,4 +200,4 @@ const buyMostProfitableUpgrades = async ({
   }
 };
 
-module.exports = { buyMostProfitableUpgrades };
+module.exports = { buyMostProfitableUpgrades, getMostProfitableUnavailableUpgrades };
